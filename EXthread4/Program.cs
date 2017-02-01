7c938e8523779b2bac74fc35bbf3066cf1c15c45
atma0004 <atma0004@edu.eal.dk>
2017-02-01 22:33:23 +0100

    Add project files.

---
+++ EXthread4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace EXthread4
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Thread Temp = new Thread(temp);
+            Temp.Start();
+             Temp.Join();
+            }
+        public static void temp()
+            {
+            string a = "C";
+            for (int counter = 0; counter<5; counter++)
+                {
+                Random temprature = new Random();
+                int number = temprature.Next(-20 , 120);
+
+                if (number<0)
+                    {
+                    Console.Write(a+number);
+                    Console.Write("      temprature is low ");
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Beep();
+                    }
+                else if (number>100)
+                    {
+                    Console.Write(a+number);
+                    
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Write("      temprature is too high ");
+                    }
+                Console.WriteLine(a+number);
+                Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+
+
+    
